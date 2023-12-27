0279522108a41753c789032e8955c1aa633ab713
sremp <sashre@protonmail.com>
2023-12-27 11:45:53 -0700

    cleanup install script, add zshrc

--- attune.sh
+++ attune.sh
@@ -10,7 +10,7 @@
 ##### Steps
 # 1. check OS
 # 2. check attune or re-attune (flag?)
-# 3. identify user + homedir
+# 3. identify user + {HOME}
 # 4. install apps (array w/ apps based on OS + flag?)
 # 5. copy relevant configs from attunement
 # 6. generate SSH + GPG if fresh attune or custom set (unique filenames needed)
@@ -24,8 +24,6 @@
 ### TBD
 
 
-
-
 ### GPG config
 # check if key already exists
 # output format:
@@ -33,24 +31,34 @@
 # $GPG_SECID = $(gpg --list-secret-keys --keyid-format=long | grep sec | sed <grab ID after />)
 # TODO: add GPG via GH API???
 
-# Identify user
-USER=$(whoami)
-HOMEDIR="/Users/$USER/"
-CONFIGDIR="$HOMEDIR/.config/"
+config_dir="$HOME/.config"
 
 # Create config directory if not found
-if [ ! -d "$HOMEDIR/.config" ]; then
-  echo "Config directory not found. Creating $CONFIGDIR"
+if [ ! -d "${config_dir}" ]; then
+  echo "${config_dir} not found. Creating..."
+  mkdir ${config_dir}
+  echo "Config directory ${config_dir} created."
+fi
+
+# Copy .oh-my-zsh submodule
+echo "Installing Oh My Zsh..."
+if [ -d "${HOME}/.oh-my-zsh" ]; then
+  echo "Overwrite existing Oh My Zsh installation? Y/[n]"
+else
+  echo "Installing Oh My Zsh from submodule... (${HOME}/.oh-my-zsh)"
+  cp -r ./.oh-my-zsh ${HOME}/
 fi
 
 # Copy ranger config
-if [ -d "$CONFIGDIR/ranger" ]; then
-  if [ -f "$CONFIGDIR/ranger/rc.conf" ]; then
-    echo "Overwrite existing Ranger config? (Y/n)"
+echo "Copying ranger config..."
+if [ -d "${config_dir}/ranger" ]; then
+  if [ -f "${config_dir}/ranger/rc.conf" ]; then
+    echo "Overwrite existing Ranger config? Y/[n]"
   else
-    echo "Applying Ranger config... ($CONFIGDIR/ranger/rc.conf)"
-    cp ./.config/ranger/rc.conf $CONFIGDIR/ranger/rc.conf
+    echo "Applying Ranger config... (${config_dir}/ranger/rc.conf)"
+    cp ./.config/ranger/rc.conf ${config_dir}/ranger/rc.conf
   fi
 else
-  cp -r ./config/ranger $CONFIGDIR
-fi 
+  echo "Creating Ranger config directory..."
+  cp -r ./config/ranger ${config_dir}
+fi
